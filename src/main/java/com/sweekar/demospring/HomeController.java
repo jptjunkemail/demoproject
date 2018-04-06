f0ebb1690d2697e83b40bac39fbde5c865535d6e
home <home@home-linux>
2018-04-06 02:39:05 -0500

    test

--- src/main/java/com/sweekar/demospring/HomeController.java
+++ src/main/java/com/sweekar/demospring/HomeController.java
@@ -32,6 +32,7 @@ public class HomeController {
 		String formattedDate = dateFormat.format(date);
 		
 		model.addAttribute("serverTime", formattedDate );
+		model.addAttribute("serverTime", formattedDate );
 		
 		logger.info("Test");
 		
